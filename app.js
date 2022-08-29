4eb68ffff98166872d9d22ffd2f3a84d15d38b34
modMars <malejandrosalcido@gmail.com>
2022-08-29 16:08:15 -0600

    Add 4x4 grid

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 const container = document.querySelector('.container');
+
+function createGrid(){
+    for(let i = 0; i<16; i++)
+    {
+        const grid = document.createElement('div');
+        container.append(grid);
+    }
+}
+function paint(){
+    this.setAttribute('style', 'background-color: black')
+}
+
+createGrid();
+let grid = container.getElementsByTagName('div');
+let gridList = Array.from(grid);
+
+for(let i = 0; i < gridList.length; i++)
+{
+    gridList[i].addEventListener('mouseover', paint);
+}
