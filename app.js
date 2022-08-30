570d364fb9d52c7f64f3ad822f182574cf4d076c
modMars <malejandrosalcido@gmail.com>
2022-08-29 18:45:40 -0600

    Add different functions I added multiple functions that let the user change the size of the grid via a button that triggers a prompt, i also added a reset button

--- app.js
+++ app.js
@@ -1,24 +1,59 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('.userInput')
+const resetbtn = document.querySelector('.reset')
+createGrid(10,10);
+let grid = container.getElementsByTagName('div');
+let gridList = Array.from(grid);
+gridList.forEach(div => {
+    div.addEventListener('mouseover', paint);
+});
+
 function createGrid(x,y){
     for(let i = 0; i<x*y; i++)
     {
         const grid = document.createElement('div');
         const growx =  1000 / x ;
-        const growy = 1000/y;
-        grid.setAttribute('style', `width: ${growx}px; height:${growy}px;`)
+        const growy = 1000 / y;
+        grid.setAttribute('style', `width: ${growy}px; height:${growy}px;`)
         container.append(grid);
     }
+    return 
 }
 function paint(){
     this.style.backgroundColor = "black";
 }
-createGrid(100,100);
-let grid = container.getElementsByTagName('div');
-let gridList = Array.from(grid);
-
-
-
-for(let i = 0; i < gridList.length; i++)
-{
-    gridList[i].addEventListener('mouseover', paint);
+function changeGrid(){
+    let input = prompt("Please write the size of the grid")
+    if(input > 100)
+    {
+        alert("Too big! please write a number under or equal to 100")
+        return;
+    }
+    else if(input <= 0)
+    {
+        alert("Too small! please write a number over 0")
+        return;
+    }
+    {
+    var divs = container.querySelectorAll('div');
+    divs.forEach(div => {
+        div.remove();
+    });
+    createGrid(input, input);
+    grid = container.getElementsByTagName('div');
+    gridList = Array.from(grid);
+    gridList.forEach(div => {
+        div.addEventListener('mouseover', paint);
+    });
+    }
 }
+
+
+
+button.addEventListener('click', changeGrid)
+resetbtn.addEventListener('click', () => {
+    var divs = container.querySelectorAll('div')
+    divs.forEach(div => {
+        div.style.backgroundColor="white";
+    });
+})
